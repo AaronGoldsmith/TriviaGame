5fb1e8a0c0120f5441c0e9ca27fa08dc8f478dda
Aaron Goldsmith <agoldsmith@scu.edu>
2018-08-09 23:57:18 -0700

    fixed a timing issue, changed 3 1D arrays into an Array of objects

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,49 +3,48 @@ var chosen = false;
 var remaining= 30;
 var playing = false;
 var countdown;
+var pause;
 
 var Questionlist = [
     {question:"Which of the following does not belong?",
-    choiceList:["Bengal","Siamese","Aegean","Borzoi"], ans: 3},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}];
-var qList = ["Question #1","Question #2","Question #3","Question #4","Question #5","Question #6","Question #7","Question #8","Question #9","Question #10"]
-var choiceList = [
-    ["Choice 1","Choice 2", "Choice 3","Choice 4"],
-    ["Choice 1b","Choice 2b", "Choice 3b","Choice 4b"],
-    ["Choice 1c","Choice 2c", "Choice 3c","Choice 4c"],
-    ["Choice 1d","Choice 2d", "Choice 3d","Choice 4d"],
-    ["Choice 1e","Choice 2e", "Choice 3e","Choice 4e"],
-    ["Choice 1f","Choice 2f", "Choice 3f","Choice 4f"],
-    ["Choice 1g","Choice 2g", "Choice 3g","Choice 4g"],
-    ["Choice 1h","Choice 2h", "Choice 3h","Choice 4h"],
-    ["Choice 1i","Choice 2i", "Choice 3i","Choice 4i"],
-    ["Choice 1j","Choice 2j", "Choice 3j","Choice 4j"],
-]
+     choiceList:["Bengal","Siamese","Aegean","Borzoi"], ans: 3},
+    {question:"Which organ can regenerate itself?",
+     choiceList:["Liver","Appendix","Brain","Gallbladdaer"], ans: 0},
+    {question: "What is the length of a marathon? (In miles)",
+     choiceList:["9.5","16","26","30"], ans: 2},
+    {question:"What is the youngest one must be to run for president?",
+     choiceList:["N/A","25","35","37"], ans: 2},
+    {question:"Which of the following rappers is made up?",
+     choiceList:["Lil Snupe","Lil Pump","Lil Xan","Lil Stoop"], ans: 3},
+    {question:"23 individuals sign up for an experiment. What is the probability two people in the group will share the same birthday",
+      choiceList:["5%","99%","20%","50%"],ans: 3},
+    {question:"Which item will be the last to hit the ground if dropped in a vaccum (no air resistance)?",
+      choiceList:["Bowling Ball","Tennis Ball","Feather","None. All hit ground at identical time"], ans: 3},
+    {question: "What does an average Game of Thrones episode cost to make?", 
+    choiceList:["600k","2million","6million","20million"],ans:2},
+    {question:"How many wives has Trump had?", choiceList:["5","2","1","3"], ans:3},
+    {question:"The Late Show _______",
+     choiceList:["with Trevor Noah","with Steven Colbert","with Jerry Seinfeld","with John Oliver"], ans: 1}
+];
+
 var ans = [];
 var INDEX = 0;
 function nextQuestion(){
-    ++INDEX;
-    // remaining
+    setUpQuestion();
+
+    // clear out selected button
+    $(".fas").toggleClass("far fas");
+    remaining = 30;
     clearInterval(countdown);
-    setUpQuestion();
+    countdown = setInterval(tick, 1000);
 }
-function prevQuestion(){
-    --INDEX;
-    setUpQuestion();
-}
+
 function setUpQuestion(){
     // update question field
-    $("#question").text(qList[INDEX]);
+    ++INDEX;
+    $("#question").text(Questionlist[INDEX].question);
     // get the list of options to choose from
-    var options = choiceList[INDEX];
+    var options = Questionlist[INDEX].choiceList;
     // select for all the rows in the table
     var tableRows = $("tbody tr")
     var choiceLabels = tableRows.find(".choice");
@@ -71,18 +70,18 @@ function isSelected(btn){
 $(document).ready(function(){
     // RADIO BUTTON click
     $("tr").on("click",function(){
-        if(isSelected($(this))||!chosen){
            toggleRadio($(this));
-        } 
-        else if(chosen){
+            // switch question after a 1 second delay
+            // $(".fas").toggleClass("far fas");
+
+            pause = setTimeout(nextQuestion,1000);
+
             // SWITCHING CHOICE OF RADIO BUTTON
-            $(".fas").toggleClass("far fas");
-            $(this).find("i").toggleClass("far fas");
-        }
+            chosen = !chosen;
+            // $(this).find("i").toggleClass("far fas");
+        });
     });
 
-   
-})
 document.onkeyup = function(event){
     var keyVal = event.key;
     
@@ -92,8 +91,7 @@ document.onkeyup = function(event){
     else if(keyVal==="ArrowRight" && INDEX <= 10){    
         ++INDEX;
         remaining = 30;
-        setUpQuestion();
-
+        nextQuestion();
     }
 }
 function run() {
